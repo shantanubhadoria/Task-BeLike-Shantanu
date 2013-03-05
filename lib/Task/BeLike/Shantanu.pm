abd68a87c88d07fd06c3b308b234f93234f9f503
Shantanu Bhadoria <shantanu@cpan.org>
2013-03-05 22:54:40 +0800

    Moved to Shantanu Plugin bundle for Dist::Zilla

--- lib/Task/BeLike/Shantanu.pm
+++ lib/Task/BeLike/Shantanu.pm
@@ -1,7 +1,43 @@
 use strict;
 use warnings;
 package Task::BeLike::Shantanu;
-
+# PODNAME: Task::BeLike::Shantanu
 # ABSTRACT: All my default Modules in a perl installation
+# VERSION
+# Dependencies
+use Acme::CPANAuthors::India;
+use Dist::Zilla::PluginBundle::SHANTANU;
+use File::UStore;
+use Pod::Weaver::PluginBundle::SHANTANU;
+use Printer::Thermal;
+
+use autodie;
+use App::cpanminus;
+use App::cpanoutdated;
+use Catalyst 5.90000;
+use Data::Dumper 2.14;
+use Device::SerialPort;
+use DBIx::Class;
+use Digest::MD5;
+use Dist::Zilla 4.300000;
+use ExtUtils::MakeMaker 6.60;
+use ExtUtils::ParseXS 3.10;
+use File::ChangeNotify 0.23;
+use File::Copy 5.16;
+use File::Find::Rule 0.33;
+use File::HomeDir 1.00;
+use File::Spec 3.40;
+use Geo::IP;
+use IO::Handle;
+use IO::Socket;
+use JSON::XS; 2.33;
+use Log::Log4perl 1.40;
+use Moose 2.06;
+use Net::IP;
+use Perl::Critic;
+use Perl::Tidy;
+use Starman 0.30;
+use YAML 0.84;
+use YAML::XS 0.38;
 
 1;
