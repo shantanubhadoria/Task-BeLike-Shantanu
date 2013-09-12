bdc5b6a25da6eb7e4f5b9220a47931c3b4c08625
Shantanu <shantanu@cpan.org>
2013-09-12 17:45:21 +0700

    refactor distro

--- lib/Task/BeLike/Shantanu.pm
+++ lib/Task/BeLike/Shantanu.pm
@@ -5,6 +5,8 @@ package Task::BeLike::Shantanu;
 # ABSTRACT: All my default Modules in a perl installation
 # VERSION
 # Dependencies
+
+use 5.010;
 use Acme::CPANAuthors::India;
 use Dist::Zilla::PluginBundle::SHANTANU;
 use File::UStore;
@@ -14,10 +16,23 @@ use Printer::Thermal;
 use autodie;
 use App::cpanminus;
 use App::cpanoutdated;
+use Authen::Passphrase;
 use Catalyst 5.90000;
+use Catalyst::Model::Helper::S3;
+use Catalyst::Plugin::Assets;
+use Catalyst::Plugin::Authentication;
+use Catalyst::Plugin::Session;
+use Catalyst::Plugin::Session::Store::FastMmap;
+use Catalyst::Plugin::StatusMessage;
+use Catalyst::Runtime;
 use Data::Dumper 2.14;
+use DateTime::Format::MySQL;
 use Device::SerialPort;
 use DBIx::Class;
+use DBIx::Class::Loader;
+use DBIx::Class::PassphraseColumn;
+use DBIx::Class::Validation;
+use DBIx::Class::InflateColumn::Authen::Passphrase;
 use Digest::MD5;
 use Dist::Zilla 4.300000;
 use ExtUtils::MakeMaker 6.60;
@@ -27,6 +42,7 @@ use File::Copy 2.20;
 use File::Find::Rule 0.33;
 use File::HomeDir 1.00;
 use File::Spec 3.40;
+use HTML::FormFu::Constraint::DBIC::Unique;
 use IO::Handle;
 use IO::Socket;
 use JSON::XS; 2.33;
